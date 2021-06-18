39443b8a14cde227facb074195bca80f1b69345f
Caio Novais <caionfds@gmail.com>
2021-06-18 20:20:18 -0300

    Add search input in home page
    Signed-off-by: Caio Novais <caionfds@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Jadno Barbosa <jadno.abs@usp.br>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="home-page">
     <nav>
       <div class="nav-title">
         <h1>Cadê o link?</h1>
@@ -13,13 +13,40 @@
       </div>
     </nav>
 
-    <div class="home">Welcome!</div>
+    <main>
+      <div class="main-container">
+        <input type="text" class="home-search-input" placeholder="Pesquisar" />
+        <div>
+          <div
+            class="subject"
+            v-for="subject in subjectGroups"
+            v-bind:key="subject.id"
+          >
+            {{ subject.name }}
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data: () => ({
+    subjectGroups: [
+      {
+        id: "1",
+        name: "calculo 1",
+        description: "Materia mais legal do mundo",
+      },
+      {
+        id: "2",
+        name: "introdução a programação",
+        description: "Devagar",
+      },
+    ],
+  }),
 };
 </script>
 
@@ -41,5 +68,41 @@ nav {
 
 .nav-options ul li {
   margin-right: 20px;
+}
+
+#home-page {
+  height: 100vh;
+}
+
+main {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin-top: 30px;
+}
+
+.home-search-input {
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  padding: 4px 20px;
+  background: #aaaaaa;
+  margin-bottom: 20px;
+}
+
+.home-search-input::placeholder {
+  color: #000000;
+}
+
+.subject {
+  padding: 10px 8px;
 }
 </style>
