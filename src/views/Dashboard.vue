3c6be22cd54d34bf3fafeda8b373e3ede6ec2f4d
JadnoABS <jadno.abs@usp.br>
2021-07-16 19:52:15 -0300

    Fix subject empty list error
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Caio Novais <caionfds@gmail.com>
    Signed-off-by: JadnoABS <jadno.abs@usp.br>

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -19,7 +19,7 @@
         </div>
       </aside>
       <main>
-        <SubjectContent :subject="subjects[currentSubject]" />
+        <SubjectContent v-if="subjectsNotEmpty" :subject="subjects[currentSubject]" />
       </main>
     </div>
   </div>
@@ -42,7 +42,7 @@ export default {
   }),
   async created() {
     const userID = this.$route.params.id;
-    console.log(userID);
+
     const response = await fetch(
       `http://localhost:3000/users/${userID}/subjects`
     );
@@ -61,6 +61,9 @@ export default {
         return this.currentSubject === index ? "item-clicked" : "";
       };
     },
+    subjectsNotEmpty() {
+      return this.subjects.length > 0;
+    }
   },
 };
 </script>
